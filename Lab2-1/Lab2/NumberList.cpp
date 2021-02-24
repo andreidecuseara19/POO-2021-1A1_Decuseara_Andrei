541f3d39722e2a35d64ee7142452b37e473e3b5f
Andrei Decuseara <andrei_decuseara@yahoo.com>
2021-02-24 13:21:10 +0200

    add

--- Lab2-1/Lab2/NumberList.cpp
+++ Lab2-1/Lab2/NumberList.cpp
@@ -4,3 +4,14 @@ void NumberList::Init()
 {
 	this->count = 0;
 }
+
+bool NumberList::Add(int x)
+{
+	if (this->count >= 10)
+		return false;
+	else
+	{
+		this->numbers[count] = x;
+		this->count++;
+	}
+}
